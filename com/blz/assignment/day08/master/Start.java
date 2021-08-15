6701eca91dfadf837b2b105217122fe38c65073b
Saurabh <saurabhdesale007@gmail.com>
2021-08-15 18:18:32 +0530

    com/blz/assignment/day08/master

---
+++ com/blz/assignment/day08/master/Start.java
@@ -0,0 +1,7 @@
+package com.blz.assignment.day08.master;
+
+public class Start {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+	}
+}
